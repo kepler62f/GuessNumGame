593eccacd4b83e0abfc5a06811a44263bd87571f
Dominic Phua <dominikphua@gmail.com>
2017-06-23 11:28:08 +0800

    Completed Game!

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-var randomizedNumber = randomNumber(2,2)
-var input = prompt('Guess a number from 1 to 100!')
-
+var randomizedNumber = randomNumber(1, 10)
 function randomNumber (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
-
-function updateH1(newH1) {
+//
+function updateH1 (newH1) {
   var h1 = document.querySelector('h1')
   h1.textContent = newH1
 }
 
-function numberChecker() {
+function numberChecker () {
+  var input = prompt('Guess a number between 1 to 10!')
   if (input > randomizedNumber) {
-    updateH1('Your answer is too high!')
+    updateH1("Your answer is too high!")
   } else {
     if (input < randomizedNumber) {
-      updateH1('Your answer is too low!')
+      updateH1("Your answer is too low!")
     } else {
-      updateH1('Correct answer!')
+      updateH1("Correct answer!")
     }
   }
 }
 
-element.addEventListener("click", numberChecker);
+// numberChecker()
+// console.log(randomizedNumber)
+// console.log(input)
 
+var button = document.createElement('button')
+
+button.innerHTML = 'Guess'
+
+var buttonLocation = document.getElementsByTagName('h3')[0]
+buttonLocation.appendChild(button)
+
+button.addEventListener("click", numberChecker)
